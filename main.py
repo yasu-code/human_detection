ef729ef708fa0f107f6e6033c5a5d11795470227
yasu-code <61762931+yasu-code@users.noreply.github.com>
2024-11-04 23:30:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import urllib.request
 
 
 def lambda_handler(event, context):
-    print(event)
+    #print(event)
     body = json.loads(event['body'])
     if body.get('check') == True:
         # 安否確認
         check_time()
+    elif body.get('test') == True:
+        push_line_message_test()
     else:
         deviceType = body.get('context').get('deviceType')
         if deviceType == 'WoPresence':
@@ -24,7 +26,8 @@ def update_detection_time():
     # 検知時間の更新(環境変数を使用)
     #===========================
     now = datetime.datetime.now()
-    print(now + datetime.timedelta(hours=9))
+    now_JST = now + datetime.timedelta(hours=9)
+    print('動態検知: ' + now_JST.strftime('%Y-%m-%d %H:%M:%S'))
     lambda_client = boto3.client('lambda')
     # 環境変数全てを指定しないと消えてしまうので注意
     lambda_client.update_function_configuration(
@@ -32,6 +35,7 @@ def update_detection_time():
         Environment={
             'Variables': {
                 'PREVIOUS_DETECTION_TIME': now.strftime('%Y-%m-%d %H:%M:%S'),
+                'PREVIOUS_DETECTION_TIME_JST': now_JST.strftime('%Y-%m-%d %H:%M:%S'),
                 'LINE_USER_ID': os.environ.get('LINE_USER_ID'),
                 'LINE_SECRET_TOKEN': os.environ.get('LINE_SECRET_TOKEN')
             }
@@ -57,6 +61,25 @@ def push_line_message():
     previous_detection_time_jst = datetime.datetime.strptime(os.environ.get('PREVIOUS_DETECTION_TIME'), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=9)
     message = [
         {'type':'text','text':previous_detection_time_jst.strftime('%Y/%m/%d %H:%M:%S') + '以降から動態検知がありません。安否確認してください。'}
+    ]
+    user_id = os.environ.get('LINE_USER_ID')
+    token = os.environ.get('LINE_SECRET_TOKEN')
+    payload = {'to': user_id, 'messages': message}
+    headers = {'content-type': 'application/json', 'Authorization': f'Bearer {token}'}
+    url = 'https://api.line.me/v2/bot/message/push'
+
+    req = urllib.request.Request(url, json.dumps(payload).encode(), headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+    print("send OK")
+
+def push_line_message_test():
+    #===========================
+    # LINEへのPUSHメッセージ作成
+    #===========================
+    print('test')
+    message = [
+        {'type':'text','text': '【テスト】送信テストです。'}
     ]
     user_id = os.environ.get('LINE_USER_ID')
     token = os.environ.get('LINE_SECRET_TOKEN')
